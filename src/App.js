3eadbc8b6eff033e7b71661b4e8db97fb5013188
mandresp <mandresperez.uk@gmail.com>
2023-06-21 04:25:31 +0000

    Update 22 files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Form from "./components/Form";
@@ -9,19 +10,22 @@ import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('About');
+  const [currentPage, setCurrentPage] = useState("Home");
 
   const renderPage = () => {
-    if (currentPage === 'About') {
+    if (currentPage === "Home") {
+      return <Home />;
+    }
+    if (currentPage === "About") {
       return <About />;
     }
-    if (currentPage === 'Portfolio') {
+    if (currentPage === "Portfolio") {
       return <Portfolio />;
     }
-    if (currentPage === 'Contact') {
+    if (currentPage === "Contact") {
       return <Form />;
     }
-    if (currentPage === 'Resume') {
+    if (currentPage === "Resume") {
       return <Resume />;
     }
   };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import './style.css';
-import Navbar from '../Navbar/Navbar';
+import React from "react";
+import "./style.css";
+import Navbar from "../Navbar/Navbar";
 
-function Header ({currentPage, handlePageChange}) {
-    return (
-        <header className="header">
-            <h1>Marcelo <span className="Andres">Andrés Pérez</span></h1>
-            <Navbar currentPage={currentPage} handlePageChange={handlePageChange}/>
-        </header>
-    )
+function Header({ currentPage, handlePageChange }) {
+  return (
+    <header className="header">
+      <h1>
+        marcelo <span className="Andres">andrés pérez</span>
+      </h1>
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+    </header>
+  );
 }
 
 export default Header;
